e070b4429a0e8769cdf75c5f0298b139784e5dd3
unknown <deekshabisht16@gmail.com>
2022-10-18 12:30:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React , {useState, useEffect} from 'react'
 
 function App() {
+  const[users,setUsers] = useState([])
+  const[text,setText] = useState([])
+  const[suggestions,setSuggestions] = useState([])
+  useEffect(()=>{
+    fetch('https://reqres.in/api/users?page=2')
+    .then((response)=>response.json)
+    .then((result)=>
+    setUsers(result.data.data))
+  },[])
+
+  function onChangeHandler(text){
+  
+    let matches = []
+    if(text.length > 0){
+      matches = users.filter(user => {
+        const regex = new RegExp('${text}',"gi");
+        return user.email.match(regex)
+      })
+    }
+    console.log('matches', matches)
+    setSuggestions(matches)
+    setText(text)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>{text} </div>
+    <input type="text" onChange={e=>onChangeHandler(e.target.value)} value={text}/>
     </div>
   );
 }
